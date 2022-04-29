3f40cc1c8674cf6b727441418b82b13ba59ea8f4
penty7710 <1219002685@qq.com>
2022-04-29 17:37:56 +0800

    用户登录注册
    Signed-off-by: penty7710 <1219002685@qq.com>

---
+++ service/service_ucenter/src/main/java/com/pty/educenter/UcenterApplication.java
@@ -0,0 +1,19 @@
+package com.pty.educenter;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author : pety
+ * @date : 2022/4/29 13:44
+ */
+@ComponentScan("com.pty")
+@SpringBootApplication
+@MapperScan("com.pty.educenter.mapper")
+public class UcenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UcenterApplication.class );
+    }
+}
